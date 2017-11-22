a9b5b985dfbc2a3ea2f862cd66ec1414d69a0e64
NamJeongho <njh0906@gmail.com>
2017-11-22 17:59:55 +0900

    add game win check logic

--- src/js/object/Board.js
+++ src/js/object/Board.js
@@ -36,16 +36,59 @@ export default class Board {
             count = 0;
 
         // 가로 ← →
-        while (this.board[_y][_x - 1] == stone && x > 0) {
+        while (_x >= 0 && this.board[y][_x--] == stone) {
             count++;
         }
-        while(this.board[_y][_x++] == stone && x<this.numCol){
+        _x = x + 1;
+        while (_x < this.numCol && this.board[y][_x++] == stone) {
             count++;
         }
+
+        if (count >= 5) return true;
+
+
         // 세로 ↑ ↓
+        _x = x;
+        _y = y;
+        count = 0;
+        while (_y >= 0 && this.board[_y--][x] == stone) {
+            count++;
+        }
+        _y = y + 1;
+        while (_y < this.numRow && this.board[_y++][x] == stone) {
+            count++;
+        }
+        if (count >= 5) return true;
+
         // 대각선 ↖ ↘
+        _x = x;
+        _y = y;
+        count = 0;
+        while (_x >= 0 && _y >= 0 && this.board[_y--][_x--] == stone) {
+            count++;
+        }
+        _x = x + 1;
+        _y = y + 1;
+        while (_x < this.numCol && _y < this.numRow && this.board[_y++][_x++] == stone) {
+            count++;
+        }
+        if (count >= 5) return true;
+
         // 대각선 ↗ ↙
-        return STONE.NONE;
+        _x = x;
+        _y = y;
+        count = 0;
+        while (_x < this.numCol && _y >= 0 && this.board[_y--][_x++] == stone) {
+            count++;
+        }
+        _x = x - 1;
+        _y = y + 1;
+        while (_x >= 0 && _y < this.numRow && this.board[_y++][_x--] == stone) {
+            count++;
+        }
+        if (count >= 5) return true;
+
+        return false;
     }
 
 }
